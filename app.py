ebe4cc626c7597fee13a0493bced85655e75a5e4
MateoCaicedoW <mcaicedo@wawand.co>
2023-10-21 13:24:11 -0500

    task: fixing cors

--- app.py
+++ app.py
@@ -3,11 +3,15 @@ from src.actions.roles import role_routes
 from src.actions.products import product_routes
 from src.actions.productions import production_routes
 from config import app
-
+from dotenv import load_dotenv
+from flask_cors import CORS
 app.register_blueprint(production_routes)
 app.register_blueprint(product_routes)
 app.register_blueprint(user_routes)
 app.register_blueprint(role_routes)
 
+CORS(app, resources={r"/*": {"origins": "*"}})
+
 if __name__ == "__main__":
+    load_dotenv()
     app.run(debug=True, port=8080, host='0.0.0.0')
--- config.py
+++ config.py
@@ -2,10 +2,9 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_migrate import Migrate
-from flask_cors import CORS
+
 app = Flask(__name__)
 
-CORS(app, resources={r"/*": {"origins": "*"}})
 
 #creamos las credenciales para conectarnos a la bd
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@localhost/corte_2'
